50cda58bae16db94a2eeb8fa01d070ac0ecc9805
sks72589 <sandeep2k2656@gmail.com>
2021-08-21 23:23:19 +0530

    v 1.0

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # RakhiWish
+Developed only for Mobile
+visit uploaded code on: https://sks72589.github.io/RakhiWish/
--- javascript.js
+++ javascript.js
@@ -1,2 +1,2 @@
 // Created by Sandeep Kumar
-alert("Tap anywhere to enable music,\nImages may take time to load,\nKeep patience, Thank You!");
+alert("Better on Mobile,\nTap anywhere to enable music,\nImages may take time to load,\nKeep patience, Thank You!");
